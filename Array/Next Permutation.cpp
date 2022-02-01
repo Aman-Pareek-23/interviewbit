4baa4b132b35563c3390ec44d1134e94a43c0ca3
Aman Pareek <82153355+Aman-Pareek-23@users.noreply.github.com>
2022-02-01 14:13:03 +0530

    Create Next Permutation.cpp

---
+++ Array/Next Permutation.cpp
@@ -0,0 +1,81 @@
+***************Next Permutation****************
+Medium
+
+127
+
+20
+
+Add to favorites
+Asked In:
+MICROSOFT
+AMAZON
+Implement the next permutation, which rearranges numbers into the numerically next greater permutation of numbers for a given array A of size N.
+
+If such arrangement is not possible, it must be rearranged as the lowest possible order i.e., sorted in an ascending order.
+
+Note:
+
+1. The replacement must be in-place, do **not** allocate extra memory.
+2. DO NOT USE LIBRARY FUNCTION FOR NEXT PERMUTATION. Use of Library functions will disqualify your submission retroactively and will give you penalty points.
+Input Format:
+
+The first and the only argument of input has an array of integers, A.
+Output Format:
+
+Return an array of integers, representing the next permutation of the given array.
+Constraints:
+
+1 <= N <= 5e5
+1 <= A[i] <= 1e9
+Examples:
+
+Input 1:
+    A = [1, 2, 3]
+
+Output 1:
+    [1, 3, 2]
+
+Input 2:
+    A = [3, 2, 1]
+
+Output 2:
+    [1, 2, 3]
+
+Input 3:
+    A = [1, 1, 5]
+
+Output 3:
+    [1, 5, 1]
+
+Input 4:
+    A = [20, 50, 113]
+
+Output 4:
+    [20, 113, 50]
+    
+*************Solution**************
+vector<int> Solution::nextPermutation(vector<int> &A) {
+int n= A.size(); 
+int cnt=0;
+for(int i=n-1;i>=1;i--){
+    if(A[i]<=A[i-1]) 
+        continue;
+    cnt=1; 
+    int k=i;
+    reverse(A.begin()+i,A.end());
+    while(k<n){
+        if(A[k]>=A[i-1]) {
+        break;
+        }
+    k++;
+    }
+swap(A[k],A[i-1]);
+sort(A.begin()+i,A.end());
+break;
+}
+if(cnt==0) {
+reverse(A.begin(),A.end());
+}
+return A;
+}
+
